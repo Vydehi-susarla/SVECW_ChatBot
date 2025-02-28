68401eb122cf72bc889d9dc91004429a7c4899bb
Vydehi-susarla <158502197+Vydehi-susarla@users.noreply.github.com>
2025-02-28 11:06:09 +0530

    Update ChatBot_main.py

--- ChatBot_main.py
+++ ChatBot_main.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-st.title("Chatbot - SVECW")
+st.set_page_config(page_title="SVECW ChatBot",layout="centered")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -22,7 +22,48 @@ df["Answer"]=df["Answer"].str.lower()
 vectorizer=TfidfVectorizer()
 question_vector=vectorizer.fit_transform(df["Question"])
 
+API_key="AIzaSyCFFB9Ow6bwHUsqrVSkhYvKwqcI3I0oo28"
+genai.configure(api_key=API_key)
+model=genai.GenerativeModel('gemini-1.5-flash')
+def find_closest_question(user_query,vectorizer,question_vector,df):
+    query_vector=vectorizer.transform([user_query.lower()])
+    similarities=cosine_similarity(query_vector,question_vectors).flatten()
+    best_match_index=similarities.argmax()
+    best_match_score=similarities[best_match_index]
+    if best_match_score>0.3:
+        return df.iloc[best_match_index]['Answer']
+    else:
+        return None
+st.title("SVECW College ChatBot")
+st.write("Welcome to College Chatbot! Ask me anything about the college.")
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if prompt := st.chat_input("Say something..."):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    closest_answer=find_closest_question(prompt,vectorizer,question_vector,df)
+    if closest_answer:
+        st.session_state.messages.append({"role":"assistant","content":closest_answer})
+        with st.chat_message("assistant"):
+            st.markdown(closest_answer)
+    else:
+        try:
+            response = model.generate_content(prompt)
+            st.session_state.messages.append({"role": "assistant", "content": response.text})
+            with st.chat_message("assistant"):
+                st.markdown(response.text)
+        except Exception as e:
+            st.error(f"Sorry, I couldn't generate a response. Error: {e}")
 
-                                        
+
+
+
+
+
+
 
 
