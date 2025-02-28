e389929ac0fcfa9592f0c301598ae3c5f6b0d086
Vydehi-susarla <158502197+Vydehi-susarla@users.noreply.github.com>
2025-02-28 10:46:37 +0530

    Update ChatBot_main.py

--- ChatBot_main.py
+++ ChatBot_main.py
@@ -8,4 +8,21 @@ st.title("Chatbot - SVECW")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-csv_url="svecw_details.csv
+csv_url="svecw_details.csv"
+
+try:
+    df=pd.read_csv(csv_url)
+except Exception as e:
+    st.error(f"Failed to load the csv file. Error: {e}")
+    st.stop()
+df=df.fillna("")
+df["Question"]=df["Question"].str.lower()
+df["Answer"]=df["Answer"].str.lower()
+
+vectorizer=TfidfVectorizer()
+question_vector=vectorizer.fit_transform(df["Question"])
+
+
+                                        
+
+
